9585239eb8f2e26cc7983b936553b35ea1143ed5
RyanT2004 <ryanchau2004@gmail.com>
2023-03-09 20:28:43 -0700

    big update

---
+++ src/main/java/org/example/stack/ArrayStack.java
@@ -0,0 +1,29 @@
+package org.example.stack;
+// POSSIBLE STACKS, DON'T HAVE TO USE!!!
+
+public class ArrayStack<T> implements Stack<T> {
+    @Override
+    public boolean empty() {
+        return false;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public void push(T item) {
+
+    }
+
+    @Override
+    public int search(T t) {
+        return 0;
+    }
+}
